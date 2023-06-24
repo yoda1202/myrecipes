429c6d10a79898319e51b6debab55787a10a7f62
Ubuntu <ubuntu@ip-172-31-19-232.ap-northeast-1.compute.internal>
2023-06-24 00:49:55 +0000

    Complete Authentication System with Admin and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_22_234538) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_23_123522) do
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
   create_table "recipes", force: :cascade do |t|
--- test/integration/chefs_listing_test.rb
+++ test/integration/chefs_listing_test.rb
@@ -4,9 +4,11 @@ class ChefsListingTest < ActionDispatch::IntegrationTest
 
   def setup
     @chef = Chef.create!(chefname: "mashrur", email: "mashrur@example.com",
-                    password: "password", password_confirmation: "password")
+                        password: "password", password_confirmation: "password")
     @chef2 = Chef.create!(chefname: "john", email: "john@example.com",
-                    password: "password", password_confirmation: "password")
+                        password: "password", password_confirmation: "password")
+    @admin_user = Chef.create!(chefname: "john1", email: "john1@example.com",
+                        password: "password", password_confirmation: "password", admin: true)
   end
 
   test "should get chefs listing" do
@@ -17,7 +19,7 @@ class ChefsListingTest < ActionDispatch::IntegrationTest
   end
 
   test "should delete chef" do
-    sign_in_as(@chef2, "password")
+    sign_in_as(@admin_user, "password")
     get chefs_path
     assert_template 'chefs/index'
     assert_difference 'Chef.count', -1 do
