f7e787b3abd671dfe559352ac175c3eaf48e98bf
Ubuntu <ubuntu@ip-172-31-19-232.ap-northeast-1.compute.internal>
2023-09-05 06:17:58 +0000

    add one to many association between recipe and chef

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_05_044050) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_05_054009) do
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
     t.string "email"
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_044050) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "chef_id"
   end
 
 end
