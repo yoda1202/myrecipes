b13c2990f07af359bd2cf7a11b8d0fff535ac60f
Ubuntu <ubuntu@ip-172-31-19-232.ap-northeast-1.compute.internal>
2023-09-05 23:51:15 +0000

    implement edit and delete

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -19,8 +19,28 @@ class RecipesController < ApplicationController
       flash[:success] = "Recipe was created successfully!"
       redirect_to recipe_path(@recipe)
     else
-      render 'new'
+      render 'new', status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success] = "Recipe was updated successfully!"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    Recipe.find(params[:id]).destroy
+    flash[:success] = "Recipe deleted successfully"
+    redirect_to recipes_path, status: :see_other
   end
 
   private
