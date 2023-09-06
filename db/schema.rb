2073e41f9eb27ed7f1d52cd9a0c7c1c5f24f241f
Ubuntu <ubuntu@ip-172-31-19-232.ap-northeast-1.compute.internal>
2023-09-06 06:32:32 +0000

    Implement chef signup with tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_05_054009) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_06_053514) do
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "recipes", force: :cascade do |t|
