6f2dbcfa6be3de9182c9b228d0782b237a46d2fa
Ubuntu <ubuntu@ip-172-31-19-232.ap-northeast-1.compute.internal>
2023-09-06 13:29:16 +0000

    Add chefs destroy and tests

--- test/models/chef_test.rb
+++ test/models/chef_test.rb
@@ -71,4 +71,11 @@ class ChefTest < ActiveSupport::TestCase
     assert_not @chef.valid?
   end
 
+  test "associated recipes should be destroyed" do
+    @chef.save
+    @chef.recipes.create!(name: "testing delete", description: "testing delete function")
+    assert_difference 'Recipe.count', -1 do
+      @chef.destroy
+    end
+  end
 end
