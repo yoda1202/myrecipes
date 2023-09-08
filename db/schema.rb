16007353085efc165bfa380cab913a7a2e56edd1
Ubuntu <ubuntu@ip-172-31-19-232.ap-northeast-1.compute.internal>
2023-09-08 21:17:07 +0000

    Add ingredient model and many to many assue with recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_08_051234) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_08_204714) do
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
     t.string "email"
@@ -18,6 +18,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_08_051234) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.boolean "admin", default: false
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.integer "ingredient_id"
   end
 
   create_table "recipes", force: :cascade do |t|
